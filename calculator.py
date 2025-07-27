bc4efc4fadeb5bb30ae4424354927c976a642db6
Shashank8811 <shashankreddy1817@gmail.com>
2025-07-27 17:08:43 +0530

    Done coding!

--- calculator.py
+++ calculator.py
@@ -1,4 +1,20 @@
 num1 = int(input("Enter a number:"))
 num2 = int(input("Enter another number:"))
-operation = '[+,-,*,/]'
+#Asks user for operation
+operation = input('Please type in operation [+,-,*,/]:')
 
+
+if operation == '+':
+    sum = num1 + num2
+    print(f'The addition of {num1} and {num2} = {sum}')
+elif operation == '-':
+    sub = num1 - num2
+    print(f'The subtraction of {num1} and {num2} = {sub}')
+elif operation == '*':
+    mul = num1 * num2
+    print(f"The multiplication of {num1} and {num2} = {mul}")
+elif operation == '/':
+    div = num1 / num2
+    print(f'The division of {num1} and {num2} = {div}')
+else:
+    print("Please Enter a valid operation!")
